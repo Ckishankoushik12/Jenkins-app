5a1e4e78c3f0fe76f8c8734cb6a0866bcf37221c
Ckishankoushik <73593808+Ckishankoushik12@users.noreply.github.com>
2025-07-25 18:09:05 +0000

    Fixing App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins
+          Learn Jenkins on Udemy
         </a>
       </header>
       <p>
